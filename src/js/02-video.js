d17c8a3de23797c1a6e06dd687f274f915b824f4
SergioPanov <www.vapanov@gmail.com>
2023-04-20 11:29:00 +0200

    02-cantfix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeRef = document.querySelector('#vimeo-player');
+const iframePlayer = new Player(iframeRef);
+const TIME_STORAGE_KEY = 'videoplayer-current-time';
+
+iframePlayer.on('timeupdate', throttle(timeSet, 1000));
+
+function timeSet(data) {
+  const timeValue = data.seconds;
+  localStorage.setItem(TIME_STORAGE_KEY, timeValue);
+}
+
+iframePlayer
+  .setCurrentTime(localStorage.getItem(TIME_STORAGE_KEY))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from 'lodash.throttle';
+const formRef = document.querySelector('.feedback-form');
+const btnRef = document.querySelector('button[type="submit"]');
+const emailInputRef = document.querySelector('input[name="email"]');
+const messageTextRef = document.querySelector('textarea[name="message"]');
+const STORAGE_KEY = 'feedback-form-state';
 
+const actualObj = JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? '';
+emailInputRef.value = actualObj.email || '';
+messageTextRef.value = actualObj.message || '';
+
+let dataObj = {
+  email: actualObj.email || '',
+  message: actualObj.message || '',
+};
+
+// populate();
+
+// Input
+
+function onInputFill(e) {
+  if (e.target.name === 'email') {
+    dataObj[e.target.name] = e.target.value;
+  } else if (e.target.name === 'message') {
+    dataObj[e.target.name] = e.target.value;
+  }
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataObj));
+}
+// Submit
+const onClickSubmit = e => {
+  e.preventDefault();
+
+  if (
+    emailInputRef.value === '' ||
+    messageTextRef.value === ' ' ||
+    messageTextRef.value === ''
+  ) {
+    alert('Fill all the gaps!');
+  } else {
+    const localData = localStorage.getItem(STORAGE_KEY);
+    console.log(localData);
+    formRef.reset();
+    window.localStorage.clear();
+    dataObj = {};
+  }
+};
+
+formRef.addEventListener('input', throttle(onInputFill, 500));
+btnRef.addEventListener('click', onClickSubmit);
